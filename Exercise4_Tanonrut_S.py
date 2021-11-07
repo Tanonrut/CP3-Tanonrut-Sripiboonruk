c4b4080c8c07f5ece9f04fc2607513c7fe8ffac7
Tanonrut <tanonrut2516@gmail.com>
2021-11-07 13:18:46 +0700

    Your score index

---
+++ Exercise4_Tanonrut_S.py
@@ -0,0 +1,9 @@
+fe=75
+gb=82
+ics=70
+cp=90
+print("-----Your Score-----")
+print("Foundation English = ",fe)
+print("General Business = ",gb)
+print("Introduction to Computer Systems = ",ics)
+print("Computer Programming = ",cp)
